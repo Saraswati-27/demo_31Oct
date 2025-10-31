516a97e6adb1287d285c91bedc8641e82896c7d3
Saraswati-27 <saraswati.khude@valuedx.com>
2025-10-31 15:40:04 +0530

    next commit

--- login.py
+++ login.py
@@ -5,8 +5,8 @@ users = {
     "alice": "qwerty"
 }
 
-username = input("Username: ")
-password = input("Password: ")
+username = input("Enter Username: ")
+password = input("Enter Password: ")
 
 if username in users and users[username] == password:
     print(f"✅ Welcome, {username}!")
